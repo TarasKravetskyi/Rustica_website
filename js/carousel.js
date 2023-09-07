5647b5c65509b1d04041bcf65eb583d51e928c76
Taras Kravetskyi <ckjym@ukr.net>
2023-09-07 16:49:14 +0000

    carousel

--- js/carousel.js
+++ js/carousel.js
@@ -3,6 +3,7 @@ const prevButton = document.getElementById('prevButton');
 const nextButton = document.getElementById('nextButton');
 
 let currentIndex = 0;
+let startX = null;
 
 function showImage(index) {
     const images = document.querySelectorAll('.photo-gallery__item');
@@ -28,7 +29,37 @@ function prevImage() {
     showImage(currentIndex);
 }
 
+function handleTouchStart(event) {
+    startX = event.touches[0].clientX;
+}
+
+function handleTouchMove(event) {
+    if (startX === null) {
+        return;
+    }
+
+    const currentX = event.touches[0].clientX;
+    const deltaX = currentX - startX;
+
+    if (deltaX > 50) {
+        prevImage();
+        startX = null;
+    } else if (deltaX < -50) {
+        nextImage();
+        startX = null;
+    }
+}
+
+function handleTouchEnd() {
+    startX = null;
+}
+
 nextButton.addEventListener('click', nextImage);
 prevButton.addEventListener('click', prevImage);
 
+galleryContainer.addEventListener('touchstart', handleTouchStart);
+galleryContainer.addEventListener('touchmove', handleTouchMove);
+galleryContainer.addEventListener('touchend', handleTouchEnd);
+
 showImage(currentIndex);
+
