64e937cdbd167b78ca1188beb35f3eb370f66dc5
Taras Kravetskyi <ckjym@ukr.net>
2023-09-11 11:18:21 +0000

    burger menu photos link

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -2,3 +2,9 @@ const checkbox = document.getElementById("burger");
 checkbox.addEventListener("click", () => {
     document.body.style.overflowY = checkbox.checked === true ? "hidden" : "visible";
 });
+
+document.getElementById('burger__menu-photos').addEventListener('click', function() {
+    checkbox.checked = false;
+    document.body.style.overflowY = "visible";
+});
+
